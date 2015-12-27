ad964abe91241a646c88e2e8b191183846c7bde7
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2015-12-27 17:07:50 +0900

    add commentout JPN for E155730_2_1.java

--- E155730_2_1.java
+++ E155730_2_1.java
@@ -1,5 +1,7 @@
 package e15_1;
 
+//package名
+
 import robocode.Robot;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
@@ -7,12 +9,12 @@ import java.awt.*;
 
 public class E155730_2_1 extends Robot {
     private void move(double x, double y) {
-        double dx = x - getX();
-        double dy = y - getY();
-        double r = Math.toDegrees(Math.atan2(dx, dy)) - getHeading();
+        double mvX = x - getX();
+        double mvY = y - getY();
+        double r = Math.toDegrees(Math.atan2(mvX, mvY)) - getHeading();
 
         turnRight(r);
-        ahead(Math.sqrt(dx * dx + dy * dy));
+        ahead(Math.sqrt(mvX * mvX + mvY * mvY));
 		turnLeft(normalRelativeAngleDegrees(getHeading()));
 
  }
